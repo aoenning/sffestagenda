efce697b3f501997120abcd93f88d057f9818e18
Anderson Oenning <anderson.oenning@amaggi.com.br>
2024-12-21 22:19:58 -0400

    Terceira versão

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from "react";
 import * as s from "./styles";
 
-function Card({ key, nome, date, email, telefone, confirmacao, valor, adiantamento, observacao }) {
+function Card({
+  key,
+  nome,
+  date,
+  email,
+  telefone,
+  confirmacao,
+  valor,
+  adiantamento,
+  observacao,
+}) {
   return (
     <s.Container>
-      <h1>{nome}</h1>  
-      <h1>{date}</h1>
-      <h1>{email}</h1>
-      <h1>{telefone}</h1>
-      <h1>{confirmacao}</h1>
-      <h1>{valor}</h1>
-      <h1>{adiantamento}</h1>
-      <h1>{observacao}</h1>
+      <s.BoxTitle>
+        <s.Titulo>{nome}</s.Titulo>
+      </s.BoxTitle>
+      <s.BoxSubTitle>
+        <h1>{date}</h1>
+        <h1>{email}</h1>
+        <h1>{telefone}</h1>
+        <h1>{confirmacao}</h1>
+        <h1>{valor}</h1>
+        <h1>{adiantamento}</h1>
+        <h1>{observacao}</h1>
+      </s.BoxSubTitle>
     </s.Container>
   );
 }
--- src/Components/Card/styles.js
+++ src/Components/Card/styles.js
@@ -2,14 +2,43 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  /* transition: 0.3s; */
-  background-color: #fff;
-  width: 400px;
-  height: 400px;
+  width: 250px;
+  height: 250px;
   /* justify-content: center; */
   align-items: center;
-  flex-direction: column;
+  background-color: #fff;
+  margin: 10px 10px 10px 10px;
+  font-family: monospace;
   display: flex;
-  padding-top: 10px; 
-  margin: 10px 10px 10px 10px
+  flex-direction: column;
+`;
+
+export const BoxTitle = styled.div`  
+  width: 100%;
+  height: 30%;
+  justify-content: center;
+  align-items: center;
+  background: #000;  
+  display: flex;
+  flex-direction: column;
+`;
+
+export const BoxSubTitle = styled.div`  
+  width: 100%;
+  height: 70%;
+  justify-content: center;
+  align-items: center;
+  background: #b22222;  
+  display: flex;
+  flex-direction: column;
+`;
+
+
+
+export const Titulo = styled.text`
+  font-size: 20px;
+  color: #fff;
+  /* margin: 10px 10px 10px 10px; */
+  align-items: center;
+  justify-content: center;
 `;
